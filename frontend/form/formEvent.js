7f121574fe4aba200679f32dc2b5c6384d2ea423
zharsuke <alazharisqi@gmail.com>
2023-11-13 11:06:23 +0700

    feat: add form event onChange, onCopy, onPaste

--- frontend/form/formEvent.js
+++ frontend/form/formEvent.js
@@ -29,4 +29,36 @@ document.addEventListener('DOMContentLoaded', function() {
         console.log('Input Nama: blur');
         document.getElementById('notifikasiSisaKarakter').style.visibility = 'hidden';
     });
+
+    document.getElementById('inputCaptcha').addEventListener('change', function() {
+        console.log('inputCaptcha: change');
+        const inputCaptcha = document.getElementById('inputCaptcha').value;
+        const submitButtonStatus = document.getElementById('submitButton');
+
+        if (inputCaptcha === 'PRNU') {
+            submitButtonStatus.removeAttribute('disabled');
+        } else {
+            submitButtonStatus.setAttribute('disabled', '');
+        }
+    });
+
+    document.getElementById('formDataDiri').addEventListener('submit', function(event) {
+        const inputCaptcha = document.getElementById('inputCaptcha').value;
+
+        if (inputCaptcha === 'PRNU') {
+            alert('Selamat! Captcha anda benar!');
+        } else {
+            alert('Captcha anda salah!');
+            document.getElementById('submitButton').setAttribute('disabled', '');
+        }
+        event.preventDefault();
+    });
+
+    document.getElementById('inputCopy').addEventListener('copy', function() {
+        alert('Anda telah mengcopy sesuatu...');
+    })
+
+    document.getElementById('inputPaste').addEventListener('paste', function() {
+        alert('Anda telah mengpaste sesuatu...');
+    })
 });
