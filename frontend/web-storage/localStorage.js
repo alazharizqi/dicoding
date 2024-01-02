f6b8b9335e3f9c3331ba4b60ee234f82e810db2a
zharsuke <alazharisqi@gmail.com>
2024-01-02 23:20:30 +0700

    feat: add local storage

---
+++ frontend/web-storage/localStorage.js
@@ -0,0 +1,26 @@
+const localStorageKey = 'PRESS_FREQUENCY';
+
+if (typeof Storage !== 'undefined') {
+    if (localStorage.getItem(localStorageKey) === null) {
+        localStorage.setItem(localStorageKey, 0);
+    }
+    const incrementButton = document.querySelector('#incrementButton');
+    const clearButton = document.querySelector('#clear');
+    const countDisplay = document.querySelector('#count');
+
+    countDisplay.innerText = localStorage.getItem(localStorageKey);
+
+    incrementButton.addEventListener('click', function() {
+        let count = localStorage.getItem(localStorageKey);
+        count++;
+        localStorage.setItem(localStorageKey, count);
+        countDisplay.innerText = localStorage.getItem(localStorageKey);
+    });
+
+    clearButton.addEventListener('click', function() {
+        localStorage.removeItem(localStorageKey);
+        countDisplay.innerText = 0;
+    });
+} else {
+    alert('Sorry, your browser does not support web storage.');
+}
