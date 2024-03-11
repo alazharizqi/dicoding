599a8ef2f5eee761f5b190e5a1e9fcf0dcf57e89
zharsuke <alazharisqi@gmail.com>
2024-03-12 02:36:59 +0700

    feat: modify item storage

--- frontend/web-storage/guessNumber.js
+++ frontend/web-storage/guessNumber.js
@@ -12,7 +12,7 @@ const answerButton3 = document.getElementById('answer-3-button');
 const sessionUserAnswerField = document.getElementById('session-user-answer-field');
 const sessionUserWrongAnswerField = document.getElementById('session-user-wrong-answer-field');
 const sessionTrueAnswerField = document.getElementById('session-true-answer-field');
-const sessionUserAttemptsAmountField = documentgetElementById('session-user-attempts-amount-field');
+const sessionUserAttemptsField = document.getElementById('session-user-attempts-amount-field');
 
 // initialize function for generating answer
 function getAnswer() {
@@ -57,7 +57,70 @@ window.addEventListener('load', function () {
         alert('Sorry, your browser does not support Web Storage...')
     }
     // initialize all value of web item storage to the document elements
-    sessionUserAttemptsAmountField.innerText = sessionStorage.getItem(sessionUserAttemptsKey);
+    sessionUserAttemptsField.innerText = sessionStorage.getItem(sessionUserAttemptsKey);
     localTotalVictoryField.innerText = localStorage.getItem(localTotalVictoryKey);
     localMaximumAttemptField.innerText = localStorage.getItem(localMaximumAttemptsKey);
+});
+
+playButton.addEventListener('click', function () {
+    sessionStorage.setItem(sessionAnswerKey, getAnswer());
+    sessionStorage.setItem(sessionUserIsPlayingKey, true);
+    beforeGameDisplay.setAttribute('hidden', true);
+    duringGameDisplay.removeAttribute('hidden');
+});
+
+answerButton1.addEventListener('click', function () {
+    sessionUserAnswerField.innerText += '1';
+    if (sessionUserAnswerField.innerText.length === 3) {
+        checkAnswer(sessionUserAnswerField.innerText);
+    }
+});
+
+answerButton2.addEventListener('click', function () {
+    sessionUserAnswerField.innerText += '2';
+    if (sessionUserAnswerField.innerText.length === 3) {
+        checkAnswer(sessionUserAnswerField.innerText);
+    }
+});
+
+answerButton3.addEventListener('click', function () {
+    sessionUserAnswerField.innerText += '3';
+    if (sessionUserAnswerField.innerText.length === 3) {
+        checkAnswer(sessionUserAnswerField.innerText);
+    }
+});
+
+function checkAnswer(userguess) {
+    const answer = sessionStorage.getItem(sessionAnswerKey);
+    if (userguess === answer) {
+        duringGameDisplay.setAttribute('hidden', true);
+        afterGameDisplay.removeAttribute('hidden');
+        sessionTrueAnswerField.innerText = answer;
+        updateScore();
+    } else {
+        const previousAttemptsAmount = parseInt(sessionStorage.getItem(sessionUserAttemptsKey));
+        sessionStorage.setItem(sessionUserAttemptsKey, previousAttemptsAmount + 1);
+        sessionUserAttemptsField.innerText = sessionStorage.getItem(sessionUserAttemptsKey);
+        sessionUserAnswerField.innerText = '';
+        sessionUserWrongAnswerField.innerText = userguess;
+    }
+}
+
+function updateScore() {
+    const sessionAttemptsValue = parseInt(sessionStorage.getItem(sessionUserAttemptsKey));
+    const localAttemptsValue = parseInt(localStorage.getItem(localMaximumAttemptsKey));
+    if (sessionAttemptsValue > localAttemptsValue) {
+        localStorage.setItem(localMaximumAttemptsKey, sessionAttemptsValue);
+        localMaximumAttemptField.innerText = sessionAttemptsValue;
+    }
+    const previousTotalVictoryAmount = parseInt(localStorage.getItem(localTotalVictoryKey));
+    localStorage.setItem(localTotalVictoryKey, previousTotalVictoryAmount + 1);
+    localTotalVictoryField.innerText = localStorage.getItem(localTotalVictoryKey);
+}
+
+window.addEventListener('beforeunload', function () {
+    sessionUserAnswerField.innerText = '';
+    sessionUserWrongAnswerField.innerText = '';
+    sessionStorage.setItem(sessionUserAttemptsKey, 0);
+    sessionUserAttemptsField.innerText = sessionStorage.getItem(sessionUserAttemptsKey);
 });
