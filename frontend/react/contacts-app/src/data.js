c2a8d364be0b68a95ffad27ccf31853ad1334562
zharsuke <alazharisqi@gmail.com>
2024-03-26 01:18:54 +0700

    feat: display contact list

---
+++ frontend/react/contacts-app/src/data.js
@@ -0,0 +1,18 @@
+const getData = () => {
+    return [
+        {
+            id: 1,
+            name: 'Azhar',
+            tag: 'zharsuke',
+            imageUrl: '/images/azhar.jpeg'
+        },
+        {
+            id: 2,
+            name: 'sasuke',
+            tag: 'uciha anu',
+            imageUrl: '/images/sasuke.jpg'
+        },
+    ];
+}
+
+export { getData };
